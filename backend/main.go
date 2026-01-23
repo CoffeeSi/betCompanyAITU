2e06f82af7d34569425693966d0c45fb0c75e4be
Muhamedia <78860009+Muhamedia@users.noreply.github.com>
2026-01-23 13:42:06 +0500

    Update main_CRUD.go

--- backend/main.go
+++ backend/main.go
@@ -16,6 +16,56 @@ func main() {
 		fmt.Printf("error: %s", err.Error())
 		return
 	}
+	func CreateBet(c *gin.Context) {
+	var bet Bet
+	if err := c.ShouldBindJSON(&bet); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Create(&bet)
+	c.JSON(http.StatusCreated, bet)
+}
+
+	func GetBets(c *gin.Context) {
+	var bets []Bet
+	db.Find(&bets)
+	c.JSON(http.StatusOK, bets)
+}
+
+	func GetBet(c *gin.Context) {
+	id := c.Param("id")
+	var bet Bet
+	if result := db.First(&bet, id); result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Bet not found"})
+		return
+	}
+	c.JSON(http.StatusOK, bet)
+}
+
+	func UpdateBet(c *gin.Context) {
+	id := c.Param("id")
+	var bet Bet
+
+	if err := db.First(&bet, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Bet not found"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&bet); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.Save(&bet)
+	c.JSON(http.StatusOK, bet)
+}
+
+	func DeleteBet(c *gin.Context) {
+	id := c.Param("id")
+	db.Delete(&Bet{}, id)
+	c.JSON(http.StatusOK, gin.H{"message": "Deleted successfully"})
+}
+	
 
 	// Three-Layered Architecture: handler -> service -> repository
 	repository := repository.NewRepository(database)
