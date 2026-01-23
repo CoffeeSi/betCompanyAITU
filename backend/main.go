f10e6806f9ac139d7f6df72b2479d16189314e73
hulahup4ikk <kuzn.vana@gmail.com>
2026-01-23 18:41:55 +0500

    initial commit

--- backend/main.go
+++ backend/main.go
@@ -16,6 +16,7 @@ func main() {
 		fmt.Printf("error: %s", err.Error())
 		return
 	}
+	fmt.Println("IVAN KUZNETSOV BRANCH")
 
 	// Three-Layered Architecture: handler -> service -> repository
 	repository := repository.NewRepository(database)
