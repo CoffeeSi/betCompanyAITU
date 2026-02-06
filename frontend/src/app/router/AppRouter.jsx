0f44a224f0fcf5a2276780163938ca95e53755ca
CoffeeSi <ewgenik02032006@gmail.com>
2026-02-06 18:43:18 +0500

    fix: event hook

--- frontend/src/app/router/AppRouter.jsx
+++ frontend/src/app/router/AppRouter.jsx
@@ -3,13 +3,15 @@ import { ROUTES } from "./routePaths";
 import HomePage from "@/pages/HomePage/HomePage"
 import LoginPage from "@/pages/LoginPage/LoginPage";
 import RegisterPage from "@/pages/RegisterPage/RegisterPage";
-import BetPage from "@/pages/BetPage/BetPage";
+import EventPage from "@/pages/EventPage/EventPage";
+import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 
 export const AppRouter = () => (
     <Routes>
         <Route path={ROUTES.HOME} element={<HomePage />}/>
         <Route path={ROUTES.LOGIN} element={<LoginPage />}/>
         <Route path={ROUTES.REGISTER} element={<RegisterPage />}/>
-        <Route path={ROUTES.BET} element={<BetPage />}/>
+        <Route path={ROUTES.EVENTS} element={<EventPage />}/>
+        <Route path="*" element={<NotFoundPage />}/>
     </Routes>
 );
--- frontend/src/app/router/routePaths.js
+++ frontend/src/app/router/routePaths.js
@@ -2,5 +2,5 @@ export const ROUTES = {
     HOME: "/",
     LOGIN: "/login",
     REGISTER: "/register",
-    BET: "/bet",
+    EVENTS: "/events/:id",
 };
